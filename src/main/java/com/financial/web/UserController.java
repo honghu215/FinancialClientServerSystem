9fa1921c75b1b778a3cb422f2df71a52f454e0d8
Hong Hu <huhong215@gmail.com>
2018-09-24 15:22:12 -0400

    deleted unnecessary comment

--- src/main/java/com/financial/web/UserController.java
+++ src/main/java/com/financial/web/UserController.java
@@ -14,7 +14,6 @@ import java.util.concurrent.Semaphore;
 
 /**
  *
- * @author shibin
  */
 @RestController
 public class UserController {
@@ -24,8 +23,7 @@ public class UserController {
     @Resource
     private UserMapper userMapper;
 
-    /**
-     * 查询所有用户
+    /*
      * @return
      */
     @GetMapping("/getUsers")
@@ -34,8 +32,7 @@ public class UserController {
         return users;
     }
 
-    /**
-     * 根据id 查询
+    /*
      * @param id
      * @return
      */
@@ -45,8 +42,7 @@ public class UserController {
         return user;
     }
 
-    /**
-     * 添加用户
+    /*
      * @param user
      */
     @PostMapping("/add")
@@ -54,8 +50,7 @@ public class UserController {
         userMapper.insert(user);
     }
 
-    /**
-     * 更新
+    /*
      * @param user
      */
     @PutMapping(value="update")
@@ -63,8 +58,7 @@ public class UserController {
         userMapper.update(user);
     }
 
-    /**
-     * 删除
+    /*
      * @param id
      */
     @DeleteMapping(value="/delete/{id}")
@@ -73,59 +67,54 @@ public class UserController {
     }
 
 
-    /**
-     * 1. synchronized修饰的方法 设置锁
+    /*
      * @return
      */
     @GetMapping("/test1")
     public synchronized String test1() {
 
-        logger.info("已锁住");
+        logger.info("locked");
         for (int i=0;i<10;i++) {
             logger.info(String.valueOf(i));
             try {
-                logger.info(Thread.currentThread().getName()+ "休眠5秒!");
-                //放大代码块执行完成的时间，便于观察
+                logger.info(Thread.currentThread().getName()+ "sleep for 5 seconds!");
                 Thread.sleep(5000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-        logger.info("已放开");
+        logger.info("released lock");
         return "Success";
     }
 
 
     /**
-     * 2. Semaphore是基于计数的信号量，它可以设定一个资源的总数量，基于这个总数量，多线程竞争获取许可信号，做自己的申请后归还，超过总数量后，线程申请许可，信号将会被阻塞。等到有资源时，继续执行
-     * 这里你访问第一次后立马第二次请求会显示资源占用
+     * 2.
      */
     Semaphore semaphore=new Semaphore(1);
 
     @GetMapping("/test2")
     public String test2(){
 
-        //可用资源数
+
         int availablePermits = semaphore.availablePermits();
         if(availablePermits>0){
-            logger.info("抢到资源");
+            logger.info("acquired resource");
         }else{
-            logger.info("资源已被占用，稍后再试");
+            logger.info("resource is being used, please try agian later");
             return "Resource is busy！";
         }
         try {
-            //请求占用一个资源
+            //request
             semaphore.acquire(1);
-            logger.info("资源正在被使用");
-            //放大时间，观察
+            logger.info("resource is being used");
             Thread.sleep(30000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }finally{
-            //释放一个资源
             semaphore.release(1);
         }
-        logger.info("释放资源");
+        logger.info("release resource");
         return "Success";
     }
 }
